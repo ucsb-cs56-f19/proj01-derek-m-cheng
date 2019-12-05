aff068eeeb14173a92bc737331125993700220f6
Derek <dcheng@ucsb.edu>
2019-12-04 23:14:36 -0800

    dc added lat and lon fields for hardcoded data

--- src/main/java/earthquakes/searches/EqSearch.java
+++ src/main/java/earthquakes/searches/EqSearch.java
@@ -1,15 +1,23 @@
 package earthquakes.searches;
 
 public class EqSearch {
-	EqSearch() {
-		distance = 0;
-		minmag = 0;
-	}
+	private int distance;
+	private int minmag;
+	private double lat;
+	private double lon;
+	private String location;
+
+	EqSearch() {};
 
 	public int getDistance() {return distance;}
 	public int getMinmag() {return minmag;}
+	public double getLat() {return lat;}
+	public double getLon() {return lon;}
+	public String getLocation() {return location;}
+
 	public void setDistance(int dist) {distance = dist;}
 	public void setMinmag(int mm) {minmag = mm;} 
-	private int distance;
-	private int minmag;
+	public void setLat(double latitude) {lat = latitude;}
+	public void setLon(double longitude) {lon = longitude;}
+	public void setLocation(String loc) {location = loc;}
 }
