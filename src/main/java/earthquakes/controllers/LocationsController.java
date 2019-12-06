37c48b77192ad35b42959a2a39ede116639ceab1
Derek <dcheng@ucsb.edu>
2019-12-06 00:59:01 -0800

    dc controller endpoint for delete

--- src/main/java/earthquakes/controllers/LocationsController.java
+++ src/main/java/earthquakes/controllers/LocationsController.java
@@ -1,6 +1,8 @@
 package earthquakes.controllers;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import earthquakes.osm.Place;
 import earthquakes.services.LocationQueryService;
 import earthquakes.searches.LocSearch;
@@ -66,6 +68,15 @@ public class LocationsController {
       model.addAttribute("locations", locationRepository.findAll());
       return "locations/index";
     }
+    
+    @DeleteMapping("/locations/delete/{id}")
+public String delete(@PathVariable("id") long id, Model model) {
+    Location location = locationRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Invalid courseoffering Id:" + id));
+    locationRepository.delete(location);
+    model.addAttribute("locations", locationRepository.findAll());
+    return "locations/index";
+}
 }
 
 
